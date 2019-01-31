d97735d70c996fc7e8109acba31043440a957d7c
Joshua Manuel <josh@Joshuas-MacBook-Pro.local>
2019-01-30 20:54:23 -0500

    Added more instructions in the Introduction view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,8 @@
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <HelloWorld msg="Welcome to Biblio!"/>
     <p>Biblio is an app to help you make bibliography citations. As you fill in the fields, you'll see the citation fill out in real-time!</p>
+    <p>To <b>copy</b> your bibliography, just click on it.</p>
+    <p>If you want to <b>view the template</b> used to create the citation, click on the Template button</p>
   </div>
 </template>
 
